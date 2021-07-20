1c0ef5aba78c847e688585ab0327aebd8e6565bc
Esha Khatri <84075946+EshaKhatri@users.noreply.github.com>
2021-07-20 16:36:07 +0530

    Add files via upload

---
+++ avgOfNoUsingFunctions.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float s1,s2,s3,a;
+printf("Enter the three integers:\n");
+scanf("%f%f%f",&s1,&s2,&s3);
+a=(s1+s2+s3)/3;
+printf("Average:%f\n",a);
+getch();
+}
